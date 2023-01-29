4afa4f0fbee4f73200ce7b133b37d498680d7866
Konneki <konnekdaniel@gmail.com>
2023-01-29 10:27:17 +0100

    Initial Commit

---
+++ Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Daniel Konnek on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
