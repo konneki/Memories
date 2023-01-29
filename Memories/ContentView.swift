44e994103b95c26aa332575bd4ff4b0d82f9db20
Konneki <konnekdaniel@gmail.com>
2023-01-29 11:26:02 +0100

    Figuring out the layout

--- Memories/ContentView.swift
+++ Memories/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var image = UIImage()
+    
+    let locationFetcher = LocationFetcher()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(uiImage: self.image)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.all, 4)
+                    .frame(width: 300, height: 300)
+                    .background(Color.black.opacity(0.2))
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+                    .padding(8)
+            
+            Button("Take picture") {
+                // Invoke Image Picker to take a pic and save a pin on the map with current user's location
+            }
+            Button("Start Tracking Location") {
+                self.locationFetcher.start()
+            }
+
+            Button("Read Location") {
+                if let location = self.locationFetcher.lastKnownLocation {
+                    print("Your location is \(location)")
+                } else {
+                    print("Your location is unknown")
+                }
+            }
         }
-        .padding()
     }
 }
 
---
+++ Memories/LocationFetcher.swift
@@ -0,0 +1,28 @@
+//
+//  LocationFetcher.swift
+//  Memories
+//
+//  Created by Daniel Konnek on 29.01.2023.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var lastKnownLocation: CLLocationCoordinate2D?
+
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+
+    func start() {
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastKnownLocation = locations.first?.coordinate
+    }
+}
